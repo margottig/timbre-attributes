e01443ad0ef6a957f570e47778a75fbf055d0fbd
margottig <marchelandio@gmail.com>
2024-06-09 19:17:50 +0200

    zeropad bug fix

--- b_from_hw_3_zeropad.m
+++ b_from_hw_3_zeropad.m
@@ -1,4 +1,8 @@
 function padded = zeropad(signal, n)
 % takes signal, n, returns n-times zero-padded signal
 % signal has to be a single row vector
-padded = [signal, zeros(1, (n-1)*(length(signal)))];
+[rows, cols] = size(signal);
+pad_rows = (4-1) * rows;
+pad_cols = (4-1) * cols;
+padded = [signal, zeros(rows, pad_cols); zeros(pad_rows, cols+pad_cols)];
+% padded = [signal, zeros(1, (n-1)*(length(signal)))];
